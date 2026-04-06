c55564fd69ef03815ab7903db82baed26524b1fd
Amit Singh <amitsinghofficial@outlook.com>
2026-04-06 22:48:34 +0530

    Add Prisma

---
+++ server/prisma/migrations/20260406170559_init/migration.sql
@@ -0,0 +1,61 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('admin', 'agent');
+
+-- CreateEnum
+CREATE TYPE "TicketStatus" AS ENUM ('open', 'resolved', 'closed');
+
+-- CreateEnum
+CREATE TYPE "TicketCategory" AS ENUM ('general_question', 'technical_question', 'refund_request');
+
+-- CreateEnum
+CREATE TYPE "MessageSender" AS ENUM ('customer', 'agent', 'ai');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'agent',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Ticket" (
+    "id" TEXT NOT NULL,
+    "subject" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "sourceEmail" TEXT NOT NULL,
+    "status" "TicketStatus" NOT NULL DEFAULT 'open',
+    "category" "TicketCategory",
+    "aiSummary" TEXT,
+    "aiSuggestedReply" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Ticket_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "sender" "MessageSender" NOT NULL,
+    "ticketId" TEXT NOT NULL,
+    "userId" TEXT,
+    "sentAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_ticketId_fkey" FOREIGN KEY ("ticketId") REFERENCES "Ticket"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
